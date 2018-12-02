77255206f07c9cd95ed838691f4af48e82a3e9f6
Vladimir Blagojevic <vblagoje@redhat.com>
2018-12-02 06:10:09 -0500

    Add grafana, update scripts

--- install.sh
+++ install.sh
@@ -19,18 +19,18 @@ oc apply -f 4-service-account.yaml
 oc adm policy add-scc-to-user privileged -ndemo-monitoring -z prometheus-operator
 oc adm policy add-scc-to-user privileged -ndemo-monitoring -z prometheus
 
-read -n1 -r -p "Please wait for Prometheus operator to be deployed. Confirm deployment using 'oc get pods' from another terminal windonw. Once confirmed press any key to proceed with installation..." key
+read -n1 -r -p "Please wait for Prometheus operator to be deployed. Confirm deployment using 'oc get pods' from another terminal window. Once confirmed press any key to proceed with installation..." key
 
 echo "Setting RBAC"
 
-oc apply -f 8-rbac-service-account.yaml
-oc apply -f 9-rbac-cluster-roles.yaml
-oc apply -f 10-rbac-cluster-role-binding.yaml
+oc apply -f 5-rbac-service-account.yaml
+oc apply -f 6-rbac-cluster-roles.yaml
+oc apply -f 7-rbac-cluster-role-binding.yaml
 
 echo "Creating service monitors and exposing Prometheus web console..."
 
-oc apply -f 11-service-monitors.yaml
-oc apply -f 12-expose-prometheus.yaml
+oc apply -f 8-service-monitors.yaml
+oc apply -f 9-expose-prometheus.yaml
 
 oc create route edge  --service=prometheus
 
@@ -55,13 +55,40 @@ oc expose svc/infinispan-app-http
 
 read -n1 -r -p "Now that Infinispan cluster is booting we'll expose Infinispan metrics to Prometheus. Press any key to continue..." key
 
-oc apply -f 20-service-metrics.yaml
+oc apply -f 10-service-metrics.yaml
 oc expose svc/infinispan-app-metrics
 
-oc apply -f 21-infinispan-service-monitor.yaml 
+oc apply -f 11-infinispan-service-monitor.yaml 
 
-echo "Installation done! Check all pods and statuses using Openshift CLI or web console."
+read -n1 -r -p "Infinispan installation done! Not let's install Grafana. Press any key to continue..." key
+
+oc new-app grafana/grafana:5.3.4
+oc expose svc/grafana
+
+read -n1 -r -p  "Let's install Prometheus data source and Infinispan dashboard to Grafana. Press any key to continue..." key
+
+protocol="http://"
+payload="$( mktemp )"
+cat <<EOF >"${payload}"
+{
+"name": "Promethus",
+"type": "prometheus",
+"access": "Browser",
+"url": "https://$( oc get route prometheus -n demo-monitoring -o jsonpath='{.spec.host}' )",
+"basicAuth": false,
+"withCredentials": false
+}
+EOF
+
+# setup grafana data source
+grafana_host="${protocol}$( oc get route grafana -o jsonpath='{.spec.host}' )"
+dashboard_file="./infinispan-grafana-dashboard.json"
+curl --insecure -H "Content-Type: application/json" -u admin:admin "${grafana_host}/api/datasources" -X POST -d "@${payload}"
+
+# this does not work for some reason, import manualy
+#curl --insecure -H "Content-Type: application/json" -u admin:admin "${grafana_host}/api/dashboards/import" -X POST -d "@${dashboard_file}"
 
 
-
+echo "All done! Let's log into grafana (username/password is admin/admin) at: "
+oc get route grafana
 
